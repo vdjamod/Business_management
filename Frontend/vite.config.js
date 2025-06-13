0a8740babde8e8e52b32bc5cbcdccf9dc3374cae
vipuljamod <vipuljamod122@gamil.com>
2025-06-14 01:12:34 +0530

    Done

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/API": {
-        target: "http://localhost:3000",
+        target: "https://business-management-backend-ghf2.onrender.com",
         changeOrigin: true,
         secure: false,
         rewrite: (path) => path.replace(/^\/API/, ""),
